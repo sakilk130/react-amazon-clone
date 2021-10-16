4c05dcf570988cb4a3f800b244f0c45c47b78c47
Sakil Khan <sakilk130@gmail.com>
2021-10-16 11:28:12 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from React</h1>
+    </div>
+  );
+}
+
+export default App;
