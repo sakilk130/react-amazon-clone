7867134dc98b912744115b75bb175928d8ed70ed
Sakil Khan <sakilk130@gmail.com>
2021-10-16 17:02:43 +0600

    create product component

---
+++ src/components/Product.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import '../styles/Product.css';
+
+function Product({ id, title, price, image, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt={title} />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
